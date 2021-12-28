15c144f3c806e5025f39dc6de71438454c645e24
jfqd <jfqd@blun.org>
2021-12-28 20:03:59 +0100

    only create folder if missing

--- copy/opt/qutic/var/mdata-setup/includes/05-ruby-authorized-keys.sh
+++ copy/opt/qutic/var/mdata-setup/includes/05-ruby-authorized-keys.sh
@@ -3,11 +3,11 @@
 
 if mdata-get ruby_authorized_keys 1>/dev/null 2>&1; then
   home='/home/ruby'
-  mkdir -p ${home}/.ssh
+  [[ ! -d ${home}/.ssh ]] && mkdir -p ${home}/.ssh
   echo "# This file is managed by mdata-get ruby_authorized_keys" \
     > ${home}/.ssh/authorized_keys
   mdata-get ruby_authorized_keys >> ${home}/.ssh/authorized_keys
   chmod 700 ${home}/.ssh
   chmod 640 ${home}/.ssh/authorized_keys
-  chown ruby:ruby ${home}/.ssh/authorized_keys
+  chown -R ruby:ruby ${home}/.ssh
 fi
